4ecb196ba450299454be13f6a328a6622a8b8f68
Michael Cenker <michael@visionapps.cz>
2016-04-13 13:38:05 +0200

    update event listing

--- src/Olesnica/WebBundle/Controller/EventController.php
+++ src/Olesnica/WebBundle/Controller/EventController.php
@@ -10,11 +10,26 @@ class EventController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('OlesnicaWebBundle:Event:index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+
+        $latestEvents = $em->getRepository('OlesnicaAdminBundle:Event')->getLatestEvents();
+
+        $recentEvents = $em->getRepository('OlesnicaAdminBundle:Event')->getRecentEvents(5);
+
+        return $this->render('OlesnicaWebBundle:Event:index.html.twig', array('latestEvents' => $latestEvents, 'recentEvents' => $recentEvents));
     }
 
     public function showAction(Event $event)
     {
         return $this->render('OlesnicaWebBundle:Event:show.html.twig', array('event' => $event));
+    }
+
+    public function recentAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $recentEvents = $em->getRepository('OlesnicaAdminBundle:Event')->getRecentEvents();
+
+        return $this->render('OlesnicaWebBundle:Event:recent.html.twig', array('recentEvents' => $recentEvents));
     }
 }
